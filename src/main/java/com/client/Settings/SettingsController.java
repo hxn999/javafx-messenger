7210dd522d121f9ee59afbca11f8dc9bd4010c7b
Tafsir Al Nafin <2305138@ugrad.cse.buet.ac.bd>
2025-06-27 22:24:49 +0600

    SettingsController updated

--- src/main/java/com/client/Settings/SettingsController.java
+++ src/main/java/com/client/Settings/SettingsController.java
@@ -2,10 +2,14 @@ package com.client.Settings;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 
 public class SettingsController {
+    public Button backButton;
+    public Button logoutButton;
+    public Button editProfileButton;
     @FXML
     private Label phoneNumberLabel;
     @FXML private CheckBox messageNotifCheckbox, soundNotifCheckbox;
