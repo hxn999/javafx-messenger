02f47473217e5b4be4e9ea2135b0a1ddaa687ad3
Tafsir Al Nafin <2305138@ugrad.cse.buet.ac.bd>
2025-07-09 23:13:04 +0600

    some minor updates

--- client/src/main/java/com/client/createaccount/CreateAccountController.java
+++ client/src/main/java/com/client/createaccount/CreateAccountController.java
@@ -1,12 +1,19 @@
 package com.client.createaccount;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
-public class CreateAccountController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateAccountController implements Initializable {
     @FXML
     private ImageView logoView;
 
@@ -24,5 +31,30 @@ public class CreateAccountController {
     private TextField nameField;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        URL imgUrl = getClass().getResource("/images/account-avatar.png");
+        if (imgUrl == null) {
+            System.err.println(">>> cannot find /images/account-avatar.png on classpath");
+        } else {
+            Image img = new Image(imgUrl.toExternalForm());
+            avatarPreview.setImage(img);
+        }
 
+
+        choosePicBtn.setOnAction(evt -> {
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Select Profile Photo");
+            chooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+            );
+            File chosen = chooser.showOpenDialog(avatarPreview.getScene().getWindow());
+            if (chosen != null) {
+                Image img = new Image(chosen.toURI().toString(),
+                        100, 100,   // width/height
+                        true, true);// preserve ratio + smooth
+                avatarPreview.setImage(img);
+            }
+        });
+    }
 }
